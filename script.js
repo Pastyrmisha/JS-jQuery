8b53fc54d967385bb439bb50f784c294b9e60815
Mikhail Zvendinov <34459362+Pastyrmisha@users.noreply.github.com>
2020-09-24 19:02:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
-addEventListener('load', function(e) {
-  document.querySelector('#test').innerHTML = 'Hello, world!';
-})
+//var a = "hello world";
+//document.write(a.lastIndexOf("world"));
+
+//var s = "hello,world,word";
+
+//var ar = s.split(",");
+
+//if(Array.isArray(ar)) {
+
+//document.write(ar[1]);
+//};
+
+
+var a = "hellOW,123,world,word";
+
+//var s = a.replace("world","new string");
+
+//document.write(s);
+
+//document.write(a.substr(7,5));
+
+//document.write(a.substring(0,6));
+
+//document.write(a.toUpperCase());
+
+//document.write(a.toLocaleLowerCase());
+
+//document.write(a.slice(6,-7));
+
+//document.write(a.search(/[0-9]/));
+
+//var r = (a.match(/[0-9]+/));
+
+//document.write(r);
+
+//document.write(a.fontcolor("red"));
+
+//document.write(a.link("http://google.com"));
+
+/*var c = "строка 2";
+var b = "строка 1";
+
+alert(c.localeCompare(b));
+*/
+
+//alert("\\ \\ \\\\ \\\ \'\"");
+
+//document.write('- Did Joffrey agree? \n - He did. He also said "I love using //\//\n".');
+
+//let who = "dragon's" + 'mother';
+//console.log(who);
+
+//const magic = '\nyou'
+//alert(magic[1]);
+
+
+
