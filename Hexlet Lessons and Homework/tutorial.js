724666187b3c021deb64fbd42468d06b4d34ef15
unknown <skturnercz@gmail.com>
2020-10-17 15:01:21 +0300

    урок по выбору символов из строки

---
+++ Hexlet Lessons and Homework/tutorial.js
@@ -0,0 +1,31 @@
+// const filterString = (str, sym) => {
+//     let i = 0;
+//     let result = '';
+  
+//     while (i < str.length) {
+//       if (str[i].toLowerCase() !== sym.toLowerCase()) { 
+//       result += str[i];  
+//     }
+//   i += 1;
+    
+//   }
+//   return result;
+//   }
+  
+//   const str = 'If I look back I am lost';
+//   console.log(filterString(str, 'I'));
+const makeItFunny = (text, char) => {
+  let i = 0;
+  let textCopy = '';
+  while (i < text.length) {
+    if (text % char[i] ) {
+    char[i] = char[i].toUpperCase();
+    textCopy += text[i];
+    }
+    i++;
+  }
+return textCopy;
+}
+const text = 'I never look back';
+// Каждый третий элемент
+console.log(makeItFunny(text, 3)); // "I NevEr LooK bAck"
