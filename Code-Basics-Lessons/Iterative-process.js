58cb8ed5a567e7c7803a1b6c904b38de91c0fdbc
unknown <skturnercz@gmail.com>
2020-10-17 19:15:26 +0300

    Iterative process

---
+++ Code-Basics-Lessons/Iterative-process.js
@@ -0,0 +1,16 @@
+const factorial = (n) => {
+  if (n === 0) {
+    return 1;
+  }
+
+  const iter = (counter, acc) => {
+    if (counter === 1) {
+      return acc;
+    }
+    return iter(counter - 1, counter * acc);
+  };
+
+  return iter(n, 1);
+};
+
+console.log(factorial(5));
