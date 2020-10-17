151b8181d35f298cec008b42c842ff9599816ae9
unknown <skturnercz@gmail.com>
2020-10-17 18:20:15 +0300

    правильное решение

--- Hexlet Lessons and Homework/tutorial.js
+++ Hexlet Lessons and Homework/tutorial.js
@@ -1,31 +1,39 @@
 // const filterString = (str, sym) => {
 //     let i = 0;
 //     let result = '';
-  
+
 //     while (i < str.length) {
 //       if (str[i].toLowerCase() !== sym.toLowerCase()) { 
 //       result += str[i];  
 //     }
 //   i += 1;
-    
+
 //   }
 //   return result;
 //   }
-  
+
 //   const str = 'If I look back I am lost';
 //   console.log(filterString(str, 'I'));
+
+
 const makeItFunny = (text, char) => {
   let i = 0;
   let textCopy = '';
+
   while (i < text.length) {
-    if (text % char[i] ) {
-    char[i] = char[i].toUpperCase();
-    textCopy += text[i];
+
+    if ((i + 1) % char === 0) {
+      textCopy += text[i].toUpperCase();
+    } else {
+      textCopy += text[i];
+
     }
     i++;
-  }
-return textCopy;
-}
+  };
+
+  return textCopy;
+};
+
 const text = 'I never look back';
 // Каждый третий элемент
-console.log(makeItFunny(text, 3)); // "I NevEr LooK bAck"
+console.log(makeItFunny(text, 3)); // 'I NevEr LooK bAck'
