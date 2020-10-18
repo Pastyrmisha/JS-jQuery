b5cec0905eba834a144770dbb6654382a517a1ce
unknown <skturnercz@gmail.com>
2020-10-18 14:18:37 +0300

    Declarative and Imperative programming

--- Hexlet Lessons and Homework/24-less-declarative-and-imperative-programming.js
+++ Hexlet Lessons and Homework/24-less-declarative-and-imperative-programming.js
@@ -4,21 +4,21 @@
 // Declarative
 const recursiveFactorial = (n) => {
     if (n === 1) {
-      return 1;
+        return 1;
     }
-    return n * recursiveFactorial(n-1);
-  } 
-  
+    return n * recursiveFactorial(n - 1);
+}
 
-//   Imperative
-  const factorial = (n) => {
+
+// Imperative
+const factorial = (n) => {
     let counter = 1;
-    let result  = 1;
-  
+    let result = 1;
+
     while (counter <= n) {
-      result = result * counter;
-      counter = counter + 1;
+        result = result * counter;
+        counter = counter + 1;
     }
-  
+
     return result;
-  }
+}
