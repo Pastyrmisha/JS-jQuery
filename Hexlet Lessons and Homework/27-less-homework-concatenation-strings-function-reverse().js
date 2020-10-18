77c97e3ff9937312f43ab8f9f6a6c1609945c87d
unknown <skturnercz@gmail.com>
2020-10-18 19:47:38 +0300

    concatenation-strings

---
+++ Hexlet Lessons and Homework/27-less-homework-concatenation-strings-function-reverse().js
@@ -0,0 +1,15 @@
+// function reverse();
+
+const reverse = (str) => {
+    let i = 0;
+    let result = "";
+  
+    while (i < str.length) {
+      result = str[i] + result;
+  
+      i += 1;
+    }
+    return result;
+  };
+  
+  console.log(reverse("Hello!"));
