5786d83b901e5d6572442cb15961d35dd6bea1cc
unknown <skturnercz@gmail.com>
2020-10-19 17:29:23 +0300

    second version

--- Hexlet Lessons and Homework/32-step-homework-cycle-for.js
+++ Hexlet Lessons and Homework/32-step-homework-cycle-for.js
@@ -12,3 +12,14 @@ const isPrime = (num) => {
     return true;
   };
   console.log(isPrime(21));
+
+//   function isPrime(n) {
+//     if (n < 2) return false;
+//     else if (n === 2) return true;
+//     for (let i=2; i<11; i++){
+//         if(i===n-1) return true;
+//         if(n%i === 0) return false;
+//     }
+//     return true;
+// }
+// console.log(isPrime(21));
