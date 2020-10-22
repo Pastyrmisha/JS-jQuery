9b5d725e0dffe8e79171936c9db9f23e90a62b92
pastyrMisha <skturnercz@gmail.com>
2020-10-22 23:23:11 +0300

    code refactoring

--- Hexlet Lessons and Homework/49-step-homework-capitalizes-1st-letter-every-word-in-sentence.js
+++ Hexlet Lessons and Homework/49-step-homework-capitalizes-1st-letter-every-word-in-sentence.js
@@ -1,14 +1,31 @@
 const length = (str) => str.length;
 const toUpperCase = (str) => str.toUpperCase();
 
-
-const capitalizeWord = (sentance) => {
-    let result = "";
+const countWord = (sentance) => {
+    let result = '';
     for (let i = 0; i < length(sentance); i += 1) {
+      if (sentance[i - 1] === ' ' || i === 0) {
+        result += toUpperCase(sentance[i]);
+      }
+      if (sentance[i - 1] !== ' ' && i > 0) {
         result += sentance[i];
-        
+      }
     }
     return result;
-}
+  };
 
-console.log(capitalizeWord(Hello));
+console.log(countWord('hello     hello     hello'));
+
+
+// Teacher:
+// // BEGIN
+// export default (str) => {
+//     let result = '';
+//     for (let i = 0; i < length(str); i += 1) {
+//       const shouldBeBig = (i === 0 || str[i - 1] === ' ');
+//       result += shouldBeBig ? toUpperCase(str[i]) : str[i];
+//     }
+  
+//     return result;
+//   };
+//   // END
